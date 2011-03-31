55482af2ce3837abd643d115f07b280b4923604d
Eamonn Maguire <eamonnmag@gmail.com>
2011-03-31 17:14:44 +0100

    The previous code wasn't working unless you have the xpath dependency available to you. So, I changed it to use a not so elegant approach, but one that will work without any additional dependencies.

--- package.sh
+++ package.sh
@@ -5,18 +5,34 @@
 
 #MVNOPTS="--offline"
 
-VERSION=`xpath -e '/project/version/text()' pom.xml  2>/dev/null`
+get_tag_data () {
+    local tag=$1
+    local xml_file=$2
+
+    # Find tag in the xml, convert tabs to spaces, remove leading spaces, remove the tag.
+    grep $tag $xml_file | \
+        tr '\011' '\040' | \
+        sed -e 's/^[ ]*//' \
+            -e 's/^<.*>\([^<].*\)<.*>$/\1/'
+
+}
+
+ALLVERSIONS=`get_tag_data \<version\> pom.xml  2>/dev/null`
+
+VERSIONSPLIT=(`echo $ALLVERSIONS | tr ' ' ' '`)
+# get first version number referenced. Should always be the version of the project, since it's always the first
+VERSION=${VERSIONSPLIT[0]}
 
 if [ "$VERSION" = "" ] ; then 
   echo "Couldn't extract version from pom.xml. Exiting."
 fi
   
 
-mvn $MVNOPTS -Dmaven.test.skip=true clean  assembly:assembly
+mvn $MVNOPTS -Dmaven.test.skip=true clean assembly:assembly
 
 zip --exclude .DS_STORE -r target/ISAcreator-$VERSION.zip Configurations Data "isatab files"
 cd target
 mv ISAcreator-$VERSION-jar-with-dependencies.jar ISAcreator.jar
-zip -u ISAcreator-$VERSION.zip ISAcreator.jar 
+zip -u ISAcreator-$VERSION.zip ISAcreator.jar
 
 cd ..
