e13ec1ad5447ed4c3cc0a7f4b3262a440cd29c5d
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-08-21 16:37:30 +0100

    Added class to deal with OSGi configuration

---
+++ src/main/java/org/isatools/isacreator/launch/ConfigurationUtil.java
@@ -0,0 +1,106 @@
+package org.isatools.isacreator.launch;
+
+import org.apache.felix.framework.util.FelixConstants;
+import org.apache.felix.main.AutoActivator;
+import org.isatools.isacreator.io.osgi.OSGiDependencyImport;
+import org.osgi.framework.Constants;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 21/08/2012
+ * Time: 11:56
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+final class ConfigurationUtil {
+
+    /**
+     * Creates a configuration for the Felix framework.
+     *
+     * @return a map of string, string with the configuration for the framework
+     */
+    public static Map<String, Object> createConfiguration()
+    {
+        final File cachedir = createCacheDir();
+
+        Map<String, Object> configMap = new HashMap<String, Object>();
+
+        String osgiDependencies = OSGiDependencyImport.getDependencies();
+        System.out.println("Loaded the following system packages:\n" + osgiDependencies);
+        configMap.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, OSGiDependencyImport.getDependencies());
+
+        //add plugins
+        File pluginsDirectory = new File("Plugins");
+        if (!pluginsDirectory.exists()) {
+            pluginsDirectory.mkdir();
+        } else {
+            File[] plugins = pluginsDirectory.listFiles();
+            StringBuilder toLoad = new StringBuilder();
+            for (File plugin : plugins) {
+                if (plugin.isDirectory()) {
+                    for (File jarFile : plugin.listFiles()) {
+                        if (jarFile.getName().contains(".jar")) {
+                            toLoad.append("file:").append(jarFile.getAbsolutePath()).append(" ");
+                        }
+                    }
+                } else {
+                    if (plugin.getName().contains(".jar")) {
+                        toLoad.append("file:").append(plugin.getAbsolutePath()).append(" ");
+                    }
+                }
+            }
+            configMap.put(AutoActivator.AUTO_START_PROP + ".1",
+                    toLoad.toString());
+        }
+
+        configMap.put(FelixConstants.LOG_LEVEL_PROP, "4");
+
+        if (cachedir != null)
+        {
+            configMap.put(Constants.FRAMEWORK_STORAGE, cachedir.getAbsolutePath());
+        }
+
+        return configMap;
+    }
+
+    /**
+     * Tries to create a temporay cache dir. If creation of the cache dir is successful,
+     * it will be returned. If creation fails, null will be returned.
+     *
+     * @return a {@code File} object representing the cache dir
+     */
+    private static File createCacheDir()
+    {
+        // Create a temporary bundle cache directory and
+        // make sure to clean it up on exit.
+        try
+        {
+            final File cachedir = File.createTempFile("isacreator.servicebase", null);
+            System.out.println(cachedir.getAbsolutePath());
+            cachedir.delete();
+
+
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    cachedir.delete();
+                }
+            });
+            return cachedir;
+
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+
+    }
+
+
+}
