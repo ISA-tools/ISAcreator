8112c8f2404ea949765b1ad8785397276daa12d3
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-10-09 12:51:03 +0100

    A bundle activator for GS

---
+++ src/main/java/org/isatools/isacreator/gs/GSActivator.java
@@ -0,0 +1,39 @@
+package org.isatools.isacreator.gs;
+
+import org.isatools.isacreator.gs.gui.GSAuthenticationMenu;
+import org.isatools.isacreator.gui.ISAcreator;
+import org.isatools.isacreator.gui.modeselection.Mode;
+import org.isatools.isacreator.launch.ISAcreatorCLArgs;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 08/10/2012
+ * Time: 23:10
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+public class GSActivator implements BundleActivator {
+
+    private ISAcreator main = null;
+
+    public void start(final BundleContext bundleContext) throws Exception {
+
+        Thread loadISATask = new Thread(new Runnable() {
+            public void run() {
+                main = new ISAcreator(ISAcreatorCLArgs.mode(), bundleContext, ISAcreatorCLArgs.configDir());
+                if (ISAcreatorCLArgs.mode()== Mode.GS){
+                    main.createGUI(ISAcreatorCLArgs.configDir(), ISAcreatorCLArgs.username(), ISAcreatorCLArgs.isatabDir(),new GSIdentityManager(), "org.isatools.isacreator.gs.gui.GSAuthenticationMenu");
+                }
+            }
+        });
+
+        loadISATask.start();
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
