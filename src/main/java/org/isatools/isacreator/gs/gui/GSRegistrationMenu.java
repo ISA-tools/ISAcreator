e0dd51a00fd6adf027a50e599cce01fede7fb9c9
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-10-10 14:20:53 +0100

    Currently empty class for GS registration menu

---
+++ src/main/java/org/isatools/isacreator/gs/gui/GSRegistrationMenu.java
@@ -0,0 +1,27 @@
+package org.isatools.isacreator.gs.gui;
+
+import org.isatools.isacreator.gui.menu.ISAcreatorMenu;
+import org.isatools.isacreator.gui.menu.MenuUIComponent;
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 09/10/2012
+ * Time: 23:28
+ *
+ * Menu to allow users to register to GS from ISAcreator
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+public class GSRegistrationMenu extends MenuUIComponent {
+
+    public GSRegistrationMenu(ISAcreatorMenu menu){
+        super(menu);
+    }
+
+    @Override
+    protected void createGUI() {
+        //TODO
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
