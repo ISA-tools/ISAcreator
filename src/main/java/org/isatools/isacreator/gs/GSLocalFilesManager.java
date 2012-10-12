a87dc79d4d90717f255f6187e54021bb3430a47b
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-10-12 17:32:45 +0100

    A manager for local files downloaded from GS

---
+++ src/main/java/org/isatools/isacreator/gs/GSLocalFilesManager.java
@@ -0,0 +1,52 @@
+package org.isatools.isacreator.gs;
+
+import org.isatools.isacreator.api.Authentication;
+import org.isatools.isacreator.gui.ISAcreator;
+import org.isatools.isacreator.launch.ISAcreatorCLArgs;
+import org.isatools.isacreator.utils.GeneralUtils;
+
+import java.util.List;
+
+/**
+ * Created by the ISATeam.
+ * User: agbeltran
+ * Date: 12/10/2012
+ * Time: 16:41
+ *
+ * @author <a href="mailto:alejandra.gonzalez.beltran@gmail.com">Alejandra Gonzalez-Beltran</a>
+ */
+public class GSLocalFilesManager {
+
+
+
+    public static void downloadFiles(Authentication gsAuthentication) {
+
+            if (ISAcreatorCLArgs.isatabDir()!=null || ISAcreatorCLArgs.isatabFiles()!=null){
+                //isatabDir not null or isatabFiles not null
+                String localTmpDirectory = GeneralUtils.createISATmpDirectory();
+
+                GSDataManager gsDataManager = ((GSIdentityManager)gsAuthentication).getGsDataManager();
+
+                if (ISAcreatorCLArgs.isatabDir()!=null){
+
+                    if (ISAcreatorCLArgs.isatabFiles()!=null){
+                        System.err.println("Either a directory containing the ISA-Tab dataset or the set of ISA-Tab files should be passed as parameters, but not both.");
+                        System.exit(-1);
+                    }
+
+                    gsDataManager.downloadAllFilesFromDirectory(ISAcreatorCLArgs.isatabDir(), localTmpDirectory);
+
+                }//isatabDir not null
+
+                if (ISAcreatorCLArgs.isatabFiles()!=null){
+
+                    for(String filePath: ISAcreatorCLArgs.isatabFiles()){
+                        gsDataManager.downloadFile(filePath, localTmpDirectory);
+                    }//for
+                }//if
+
+                ISAcreatorCLArgs.isatabDir(localTmpDirectory);
+            }//
+
+    }
+}
