f9e5ce84ce229780f0ee537c50fd80e049746a39
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-10-26 10:50:09 +0100

    Retrieves list of error messages

--- src/main/java/org/isatools/isacreator/gs/GSLocalFilesManager.java
+++ src/main/java/org/isatools/isacreator/gs/GSLocalFilesManager.java
@@ -1,10 +1,12 @@
 package org.isatools.isacreator.gs;
 
+import org.isatools.errorreporter.model.ErrorMessage;
 import org.isatools.isacreator.api.Authentication;
 import org.isatools.isacreator.gui.ISAcreator;
 import org.isatools.isacreator.launch.ISAcreatorCLArgs;
 import org.isatools.isacreator.utils.GeneralUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,7 +22,9 @@ import java.util.regex.Pattern;
 public class GSLocalFilesManager {
 
 
-    public static void downloadFiles(Authentication gsAuthentication) {
+    public static List<ErrorMessage> downloadFiles(Authentication gsAuthentication) {
+
+            List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
 
             if (ISAcreatorCLArgs.isatabDir()!=null || ISAcreatorCLArgs.isatabFiles()!=null){
                 //isatabDir not null or isatabFiles not null
@@ -35,19 +39,26 @@ public class GSLocalFilesManager {
                         System.exit(-1);
                     }
 
-                    gsDataManager.downloadAllFilesFromDirectory(ISAcreatorCLArgs.isatabDir(), localTmpDirectory);
+                    errors.addAll(gsDataManager.downloadAllFilesFromDirectory(ISAcreatorCLArgs.isatabDir(), localTmpDirectory));
+                    if (!errors.isEmpty()){
+                        return errors;
+                    }
 
                 }//isatabDir not null
 
                 if (ISAcreatorCLArgs.isatabFiles()!=null){
 
+
                     for(String filePath: ISAcreatorCLArgs.isatabFiles()){
-                        gsDataManager.downloadFile(filePath, localTmpDirectory);
+                        errors.add(gsDataManager.downloadFile(filePath, localTmpDirectory));
                     }//for
+
                 }//if
 
                 ISAcreatorCLArgs.isatabDir(localTmpDirectory);
-            }//
+            }// if
+
+        return errors;
 
     }
 
