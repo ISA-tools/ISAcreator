9f0c35bb502e3454ae143809bbab831b233bac97
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2013-02-07 17:32:31 +0000

    Copyright text

--- src/main/java/org/isatools/isacreator/gui/AddStudyDialog.java
+++ src/main/java/org/isatools/isacreator/gui/AddStudyDialog.java
@@ -5,11 +5,7 @@
  ISAcreator is licensed under the Common Public Attribution License version 1.0 (CPAL)
 
  EXHIBIT A. CPAL version 1.0
- <<<<<<< HEAD
- �The contents of this file are subject to the CPAL version 1.0 (the �License�);
- =======
  The contents of this file are subject to the CPAL version 1.0 (the License);
- >>>>>>> 9eb576c237c7bdc3ffbf61636aa76f3cae3d7fb9
  you may not use this file except in compliance with the License. You may obtain a
  copy of the License at http://isa-tools.org/licenses/ISAcreator-license.html.
  The License is based on the Mozilla Public License version 1.1 but Sections
@@ -17,11 +13,7 @@
  provide for limited attribution for the Original Developer. In addition, Exhibit
  A has been modified to be consistent with Exhibit B.
 
- <<<<<<< HEAD
- Software distributed under the License is distributed on an �AS IS� basis,
- =======
  Software distributed under the License is distributed on an AS IS basis,
- >>>>>>> 9eb576c237c7bdc3ffbf61636aa76f3cae3d7fb9
  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
  the specific language governing rights and limitations under the License.
 
