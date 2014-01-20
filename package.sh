061b34010f6f1b5911606b7ceea0086be4b95a9f
Eamonn Maguire <eamonnmag@gmail.com>
2014-01-20 17:28:28 +0000

    Fixed packaging script.

--- package.sh
+++ package.sh
@@ -3,6 +3,18 @@
 # More details in the POM. 
 #
 
+# should be used by passing in either scidata or isa as a parameter, e.g. ./package.sh scidata
+# switching these will result in different actions being performed on packaging.
+
+PACKAGE_TYPE=$1
+echo $PACKAGE_TYPE
+
+if [ "$PACKAGE_TYPE" = ""  ]
+then
+    PACKAGE_TYPE="isa"
+fi
+
+alias mvn='/Users/eamonnmaguire/dev/maven/bin/mvn'
 #MVNOPTS="--offline"
 get_tag_data () {
     local tag=$1
@@ -22,43 +34,73 @@ VERSIONSPLIT=(`echo $ALLVERSIONS | tr ' ' ' '`)
 # get first version number referenced. Should always be the version of the project, since it's always the first
 VERSION=${VERSIONSPLIT[0]}
 
-if [ "$VERSION" = "" ] ; then 
+if [ "$VERSION" = "" ]
+then
   echo "Couldn't extract version from pom.xml. Exiting."
 fi
-  
 
-rm -rf Configurations
+
 rm -rf src/main/resources/Configurations
 
-CONFIGURATION=isaconfig-default_v2014-01-16.zip
-
-mkdir Configurations
 mkdir src/main/resources/Configurations
-cd Configurations
+
+if [ "$PACKAGE_TYPE" = "scidata"  ]
+then
+    CONFIGURATION=isaconfig-Scientific-Data-v1.1.zip
+else
+    CONFIGURATION=isaconfig-default_v2014-01-16.zip
+fi
+
 wget https://bitbucket.org/eamonnmag/isatools-downloads/downloads/"$CONFIGURATION" --no-check-certificate
-cp $CONFIGURATION ../src/main/resources/Configurations/
+
+cp $CONFIGURATION src/main/resources/Configurations/
+
+
+WD=$(pwd)
+
+pwd
+cd src/main/resources/Configurations/
 unzip $CONFIGURATION
-rm $CONFIGURATION
-cd ../
 
-## keeping configurations in resources so that they are included in the jar
-cd src/main/resources/Configurations
-unzip $CONFIGURATION 
-rm $CONFIGURATION 
-cd ../../../..
+rm -f $CONFIGURATION
+pwd
+cd $WD
+echo "Changing back to target..."
+pwd
 
-mvn $MVNOPTS -Dmaven.test.skip=true clean assembly:assembly
+mvn $MVNOPTS -Dmaven.test.skip=true clean assembly:assembly -Pbuild
+
+
+mkdir target/Configurations
+cp $CONFIGURATION target/Configurations/
+rm -f $CONFIGURATION
+
+cd target/Configurations/
+unzip $CONFIGURATION
+rm -f $CONFIGURATION
 
 # Now package up the tools
+cd ../
 
-zip --exclude .DS_STORE -r target/ISAcreator-$VERSION-all.zip Configurations Data "isatab files"
-cd target
+mkdir "isatab files"
+if [ "$PACKAGE_TYPE" = "scidata" ]
+then
+    cd "isatab files"
+    wget https://bitbucket.org/eamonnmag/isatools-downloads/downloads/SciData-Datasets-1-and-2.zip --no-check-certificate
+    unzip SciData-Datasets-1-and-2.zip
+    rm -f SciData-Datasets-1-and-2.zip
+else
+    cp -r ../"isatab files"/* "isatab files"
+fi
+
+cd ../
+zip --exclude .DS_STORE -r ISAcreator-$VERSION-all.zip Configurations ../ProgramData "isatab files"
 mv ISAcreator-$VERSION-jar-with-dependencies.jar ISAcreator.jar
 zip -u ISAcreator-$VERSION-all.zip ISAcreator.jar
 
 python ../bundler.py
 
 echo "Packaging completed successfully!"
-cd ../
-zip --exclude .DS_STORE -r target/ISAcreator-$VERSION-all.zip target/ISAcreator.app Configurations Data "isatab files"
+
+zip --exclude .DS_STORE -r ISAcreator-$VERSION-mac.zip ISAcreator.app Configurations ../ProgramData "isatab files"
 
