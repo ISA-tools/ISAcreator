63ba8fd32814fdad2263a8f4ddd44f7d0c176c21
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2014-08-20 23:40:26 +0100

    More code for submission interface

--- src/main/java/org/isatools/isacreator/gui/submission/ENASubmissionUI.java
+++ src/main/java/org/isatools/isacreator/gui/submission/ENASubmissionUI.java
@@ -7,6 +7,9 @@ import org.isatools.isacreator.effects.FooterPanel;
 import org.isatools.isacreator.effects.GraphicsUtils;
 import org.isatools.isacreator.effects.HUDTitleBar;
 import org.isatools.isacreator.gui.ISAcreator;
+import org.isatools.isacreator.managers.ApplicationManager;
+import org.isatools.isacreator.settings.ISAcreatorProperties;
+import org.isatools.isatab.isaconfigurator.ISAConfigurationSet;
 import org.jdesktop.fuse.InjectedResource;
 import org.jdesktop.fuse.ResourceInjector;
 
@@ -14,6 +17,7 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
+import java.io.File;
 
 /**
  * User intereface for ENA submission.
@@ -27,6 +31,9 @@ public class ENASubmissionUI extends JFrame {
 
     @InjectedResource
     private Image submitIcon, submitIconInactive;
+
+    @InjectedResource
+    private ImageIcon saveISAtab;
 
     public static final float DESIRED_OPACITY = .93f;
 
@@ -57,14 +64,14 @@ public class ENASubmissionUI extends JFrame {
 
         ((JComponent) getContentPane()).setBorder(new EtchedBorder(UIHelper.LIGHT_GREEN_COLOR, UIHelper.LIGHT_GREEN_COLOR));
 
-//        Container loadingInfo = UIHelper.padComponentVerticalBox(100, new JLabel(submitENAAnimation));
-//
-//        swappableContainer = new JPanel();
-//        swappableContainer.add(loadingInfo);
-//        swappableContainer.setBorder(new EmptyBorder(1, 1, 1, 1));
-//        swappableContainer.setPreferredSize(new Dimension(750, 450));
-//
-//        add(swappableContainer, BorderLayout.CENTER);
+        Container submitInfo = UIHelper.padComponentVerticalBox(100, new JLabel(submitENAAnimation));
+
+        swappableContainer = new JPanel();
+        swappableContainer.add(submitInfo);
+        swappableContainer.setBorder(new EmptyBorder(1, 1, 1, 1));
+        swappableContainer.setPreferredSize(new Dimension(750, 450));
+
+        add(swappableContainer, BorderLayout.CENTER);
 
         FooterPanel footer = new FooterPanel(this);
         add(footer, BorderLayout.SOUTH);
@@ -75,6 +82,55 @@ public class ENASubmissionUI extends JFrame {
 
     public void submit(){
 
+        Thread performer = new Thread(new Runnable() {
+
+            public void run() {
+                log.info("Current ISA-Tab is: " + ISAcreatorProperties.getProperty(ISAcreatorProperties.CURRENT_ISATAB));
+
+                if (!new File(ISAcreatorProperties.getProperty(ISAcreatorProperties.CURRENT_ISATAB)).exists()) {
+                    log.warn("Current ISA Tab file doesn't exist in the file system...");
+                    Container saveISAtabContainer = UIHelper.padComponentVerticalBox(70, new JLabel(saveISAtab));
+                    swapContainers(saveISAtabContainer);
+                } else {
+
+                    log.info("Saving current ISAtab file");
+                    ApplicationManager.getCurrentApplicationInstance().saveISATab();
+                    log.info("ISAtab file saved");
+
+                    System.out.println("Setting config path before validation to " + ISAcreatorProperties.getProperty(ISAcreatorProperties.CURRENT_CONFIGURATION));
+
+                    ISAConfigurationSet.setConfigPath(ISAcreatorProperties.getProperty(ISAcreatorProperties.CURRENT_CONFIGURATION));
+
+
+                }
+
+
+            }});
+
+        performer.start();
+
     }
+
+        private void swapContainers(final Container newContainer) {
+
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    if (newContainer != null) {
+                        swappableContainer.removeAll();
+                        swappableContainer.add(newContainer);
+                        swappableContainer.repaint();
+                        swappableContainer.validate();
+                        swappableContainer.updateUI();
+
+                        newContainer.validate();
+                        newContainer.repaint();
+
+                        validate();
+                        repaint();
+                    }
+                }
+            });
+
+        }
 
 }
