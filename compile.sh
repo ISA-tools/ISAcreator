8ba4ad67ced2613a0c5251cc7feeade551556bf4
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2016-08-18 15:47:00 +0100

    Changes to compile scritp, leaving only one copy of BII-I-1 in the repo (under src/test/resources/test-data)

--- compile.sh
+++ compile.sh
@@ -3,17 +3,32 @@
 # It first downloads the configuration files, which are required withint the tool.
 
 SKIP_TESTS=true
+CONFIG="default"
 
-while getopts c:t option
+while getopts h,help,config:,c:,t,tests option
 do
         case "${option}"
         in
-                t) SKIP_TESTS=false;;
-                c) CONFIG=${OPTARG};;
+                t,tests) SKIP_TESTS=false;;
+                c,config) CONFIG=${OPTARG};;
         esac
 done
 
-echo "Value of SKIP_TESTS ---", $SKIP_TESTS
+if [ "$CONFIG" = "scidata"  ]
+then
+    CONFIG_FILES=isaconfig-Scientific-Data-v1.2.zip
+else
+     if [ "$CONFIG" = "mixs"  ]
+     then
+        CONFIG_FILES=isaconfig-mixs-v4.zip
+    else
+        if [ "$CONFIG" = "default"  ]
+        then
+            CONFIG_FILES=isaconfig-default_v2015-07-02.zip
+        fi
+    fi
+fi
+
 
 rm -rf src/main/resources/Configurations
 mkdir src/main/resources/Configurations
@@ -25,24 +40,43 @@ else
    exit 1
 fi
 
-CONFIGURATION=isaconfig-default_v2015-07-02.zip
 
-curl -L -O http://bitbucket.org/eamonnmag/isatools-downloads/downloads/"$CONFIGURATION"
+curl -L -O http://bitbucket.org/eamonnmag/isatools-downloads/downloads/"$CONFIG_FILES"
 
-mv $CONFIGURATION src/main/resources/Configurations/
+mv $CONFIG_FILES src/main/resources/Configurations/
 
 WD=$(pwd)
 
 pwd
 cd src/main/resources/Configurations/
-unzip $CONFIGURATION
-rm -f $CONFIGURATION
+unzip $CONFIG_FILES
+rm -f $CONFIG_FILES
 pwd
 cd $WD
-echo "Changing back to target..."
+echo "Changing back to project folder..."
 pwd
 
-
 mvn $MVNOPTS -Dmaven.test.skip=$SKIP_TESTS clean assembly:assembly -Pbuild
+
+
+mkdir "isatab files"
+if [ "$CONFIG" = "scidata" ]
+then
+    cd "isatab files"
+    curl -L -O https://bitbucket.org/eamonnmag/isatools-downloads/downloads/SciData-Datasets-1-and-2.zip
+    unzip SciData-Datasets-1-and-2.zip
+    rm -f SciData-Datasets-1-and-2.zip
+    cd ../
+fi
+if [ "$CONFIG" = "mixs" ]
+then
+    cd "isatab files"
+    curl -L -O https://bitbucket.org/eamonnmag/isatools-downloads/downloads/$MIXS_DATASETS
+    unzip $MIXS_DATASETS
+    rm -f $MIXS_DATASETS
+    cd ../
+else
+    cp -r src/test/resources/test-data/* "isatab files"/
+fi
 
 
